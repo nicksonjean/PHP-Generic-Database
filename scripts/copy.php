525eda4b79d34690d218537a319f90981508972e
Nickson Jeanmerson <nickson@q9.com.br>
2023-06-30 15:11:51 -0300

    feat: pre-commit

--- scripts/copy.php
+++ scripts/copy.php
@@ -2,14 +2,14 @@
 
 $result = [];
 foreach ($argv as $arg) {
-  $e = explode("=", $arg);
-  if (count($e) == 2) {
-    $result[$e[0]] = $e[1];
-  } else
-    $result[$e[0]] = 0;
+   $e = explode("=", $arg);
+   if (count($e) === 2) {
+      $result[$e[0]] = $e[1];
+   } else {
+      $result[$e[0]] = 0;
+   }
 }
-
 if (isset($result['--source']) && isset($result['--dest'])) {
-  copy($result['--source'], $result['--dest']);
-  chmod($result['--dest'], 0755);
+    copy($result['--source'], $result['--dest']);
+    chmod($result['--dest'], 0755);
 }
--- scripts/eraser.php
+++ scripts/eraser.php
@@ -2,21 +2,21 @@
 
 $result = [];
 foreach ($argv as $arg) {
-  $e = explode("=", $arg);
-  if (count($e) == 2)
-    $result[$e[0]] = $e[1];
-  else
-    $result[$e[0]] = 0;
+   $e = explode("=", $arg);
+   if (count($e) === 2) {
+      $result[$e[0]] = $e[1];
+   } else {
+      $result[$e[0]] = 0;
+   }
 }
-
 if (isset($result['--dest']) && is_dir($result['--dest'])) {
-  $result['--dest'] = str_replace(array('/', '\\'), DIRECTORY_SEPARATOR, $result['--dest']);
-  if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
-    $cmd = sprintf('rmdir /s /q %s', $result['--dest']);
-  } else {
-    $cmd = sprintf('rm -rf %s', $result['--dest']);
-  }
-  exec($cmd);
-  @sleep(3);
-  @rmdir('./vendor/');
+    $result['--dest'] = str_replace(array('/', '\\'), DIRECTORY_SEPARATOR, $result['--dest']);
+    if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
+        $cmd = sprintf('rmdir /s /q %s', $result['--dest']);
+    } else {
+        $cmd = sprintf('rm -rf %s', $result['--dest']);
+    }
+    exec($cmd);
+    @sleep(3);
+    @rmdir('./vendor/');
 }
