35395b43e00b5cd02f77f3a4c210e673002118aa
Nickson Jeanmerson <nickson@q9.com.br>
2023-06-30 15:12:52 -0300

    feat: pre-commit

--- scripts/eraser.php
+++ scripts/eraser.php
@@ -4,9 +4,9 @@ $result = [];
 foreach ($argv as $arg) {
    $e = explode("=", $arg);
    if (count($e) === 2) {
-      $result[$e[0]] = $e[1];
+       $result[$e[0]] = $e[1];
    } else {
-      $result[$e[0]] = 0;
+       $result[$e[0]] = 0;
    }
 }
 if (isset($result['--dest']) && is_dir($result['--dest'])) {
