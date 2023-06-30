17ea8ed8471948c974196966bf2b86cce0b527cf
Nickson Jeanmerson <nickson@q9.com.br>
2023-06-30 19:33:35 -0300

    feat: upgrade phpunit and scripts

--- tests/AttributesTest.php
+++ tests/AttributesTest.php
@@ -1,8 +1,6 @@
 <?php
 
 use GenericDatabase\Engine\PDOEngine;
-use GenericDatabase\Engine\PDO\Attributes;
-
 use PHPUnit\Framework\TestCase;
 
 final class AttributesTest extends TestCase
@@ -12,16 +10,16 @@ final class AttributesTest extends TestCase
     public static function setUpBeforeClass(): void
     {
         self::$instance = PDOEngine
-        ::setDriver('sqlite')
-        ::setDatabase('memory')
-        ::setCharset('utf8')
-        ::setOptions([
-        PDO::ATTR_PERSISTENT => true,
-        PDO::ATTR_EMULATE_PREPARES => true,
-        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ
-        ])
-        ::setException(true)
-        ->connect();
+            ::setDriver('sqlite')
+            ::setDatabase('memory')
+            ::setCharset('utf8')
+            ::setOptions([
+                PDO::ATTR_PERSISTENT => true,
+                PDO::ATTR_EMULATE_PREPARES => true,
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ
+            ])
+            ::setException(true)
+            ->connect();
     }
 
     public static function tearDownAfterClass(): void
