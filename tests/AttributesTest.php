80a17ca3f5ad03c0abc78df38ca4fe75f502fdaf
Nickson Jeanmerson <nickson@q9.com.br>
2023-06-30 19:18:31 -0300

    feat: upgrade phpunit and scripts

--- tests/AttributesTest.php
+++ tests/AttributesTest.php
@@ -2,42 +2,43 @@
 
 use
   GenericDatabase\Engine\PDOEngine,
+
   GenericDatabase\Engine\PDO\Attributes;
 
 use PHPUnit\Framework\TestCase;
 
 final class AttributesTest extends TestCase
 {
-  private static $instance;
+    private static $instance;
 
-  public static function setUpBeforeClass(): void
-  {
-    self::$instance = PDOEngine
-      ::setDriver('sqlite')
-      ::setDatabase('memory')
-      ::setCharset('utf8')
-      ::setOptions([
+    public static function setUpBeforeClass(): void
+    {
+        self::$instance = PDOEngine
+        ::setDriver('sqlite')
+        ::setDatabase('memory')
+        ::setCharset('utf8')
+        ::setOptions([
         PDO::ATTR_PERSISTENT => true,
         PDO::ATTR_EMULATE_PREPARES => true,
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ
-      ])
-      ::setException(true)
-      ->connect();
-  }
+        ])
+        ::setException(true)
+        ->connect();
+    }
 
-  public static function tearDownAfterClass(): void
-  {
-    self::$instance = null;
-  }
+    public static function tearDownAfterClass(): void
+    {
+        self::$instance = null;
+    }
 
-  public function testFetchAllReturnsArray()
-  {
-    $this->assertIsArray(self::$instance->getAttributes());
-  }
+    public function testFetchAllReturnsArray()
+    {
+        $this->assertIsArray(self::$instance->getAttributes());
+    }
 
-  public function testFetchAllRetrievesAllAttributes()
-  {
-    $result = self::$instance->getAttributes();
-    $this->assertArrayHasKey('ERRMODE', $result);
-  }
+    public function testFetchAllRetrievesAllAttributes()
+    {
+        $result = self::$instance->getAttributes();
+        $this->assertArrayHasKey('ERRMODE', $result);
+    }
 }
--- tests/PDOEngineTest.php
+++ tests/PDOEngineTest.php
@@ -2,46 +2,45 @@
 
 use
   GenericDatabase\Engine\PDOEngine;
-
 use PHPUnit\Framework\TestCase;
 
 final class PDOEngineTest extends TestCase
 {
-  private static $instance;
+    private static $instance;
 
-  public static function setUpBeforeClass(): void
-  {
-    self::$instance = PDOEngine
-      ::setDriver('mysql')
-      ::setHost('localhost')
-      ::setPort(3306)
-      ::setDatabase('demodev')
-      ::setUser('root')
-      ::setPassword('')
-      ::setCharset('utf8')
-      ::setOptions([
+    public static function setUpBeforeClass(): void
+    {
+        self::$instance = PDOEngine
+        ::setDriver('mysql')
+        ::setHost('localhost')
+        ::setPort(3306)
+        ::setDatabase('demodev')
+        ::setUser('root')
+        ::setPassword('')
+        ::setCharset('utf8')
+        ::setOptions([
         PDO::ATTR_PERSISTENT => true,
         PDO::ATTR_EMULATE_PREPARES => true,
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ
-      ])
-      ::setException(true)
-      ->connect();
-  }
+        ])
+        ::setException(true)
+        ->connect();
+    }
 
-  public function testConnect(): void
-  {
-    $this->assertInstanceOf(PDOEngine::class, self::$instance);
-  }
+    public function testConnect(): void
+    {
+        $this->assertInstanceOf(PDOEngine::class, self::$instance);
+    }
 
-  public function testGetConnection()
-  {
-    $this->assertNotEmpty(self::$instance->getConnection());
-  }
+    public function testGetConnection()
+    {
+        $this->assertNotEmpty(self::$instance->getConnection());
+    }
 
-  public function testLoadFromFile(): void
-  {
-    $this->assertGreaterThanOrEqual(1, self::$instance->loadFromFile('./tests/test.sql'));
-  }
+    public function testLoadFromFile(): void
+    {
+        $this->assertGreaterThanOrEqual(1, self::$instance->loadFromFile('./tests/test.sql'));
+    }
 
   // public function testDsn()
   // {
