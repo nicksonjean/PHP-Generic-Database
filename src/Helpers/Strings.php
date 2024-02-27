eead5653437cdeda5f067b287228fc52d518cef3
Nickson Jeanmerson <nickson.jeanmerson@gmail.com>
2024-02-27 02:31:59 -0300

    chore fix lint

--- src/Helpers/Strings.php
+++ src/Helpers/Strings.php
@@ -4,7 +4,6 @@ namespace GenericDatabase\Helpers;
 
 class Strings
 {
-
     public static function toCamelize($input, $separator = '_')
     {
         return lcfirst(str_replace($separator, '', ucwords((string) $input, $separator)));
