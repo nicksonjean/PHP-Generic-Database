f9c0f10dc21333fecee602de9e61d5cf1061005c
Nickson Jeanmerson <psmxnnickson@topaziopartner.com.br>
2025-03-16 01:24:22 -0300

    chore: create new object type for QueryBuilder named by QueryObject

--- src/Shared/Enumerator.php
+++ src/Shared/Enumerator.php
@@ -6,12 +6,12 @@ namespace GenericDatabase\Shared;
  * This trait provides utility methods for handling enum cases.
  *
  * Methods:
- * - names(): Returns an array of enum case names.
- * - values(): Returns an array of enum case values.
- * - array(): Combines enum values and names into an associative array.
- * - casesIndexedByName(): Returns an associative array of enum cases indexed by their names.
- * - isValidCase(string $name): Checks if a given name is a valid enum case.
- * - fromName(string $name): Retrieves the enum case by name, throws an exception if not found.
+ * - `names(): array:` Returns an array of enum case names.
+ * - `values(): array:` Returns an array of enum case values.
+ * - `array(): array:` Combines enum values and names into an associative array.
+ * - `casesIndexedByName(): array:` Returns an associative array of enum cases indexed by their names.
+ * - `isValidCase(string $name): bool:` Checks if a given name is a valid enum case.
+ * - `fromName(string $name): self:` Retrieves the enum case by name, throws an exception if not found.
  */
 trait Enumerator
 {
@@ -30,7 +30,7 @@ trait Enumerator
         return array_combine(self::values(), self::names());
     }
 
-    public static function casesIndexedByName()
+    public static function casesIndexedByName(): array
     {
         return array_combine(array_map(fn(self $case) => $case->name, self::cases()), self::cases());
     }
